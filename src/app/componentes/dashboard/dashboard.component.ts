d3d760bde4594b63a61ee84984da998e2448058e
unknown <maxinbusquedaydesarrollo@gmail.com>
2023-06-19 01:43:16 -0300

    agregando property binding y event

--- src/app/componentes/dashboard/dashboard.component.ts
+++ src/app/componentes/dashboard/dashboard.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+frase:String;
+txtEvent:String = "usuario";
+
+constructor(){
+  this.frase="Se usa para pasar informacion del ts al html, se suele utilizar para hacer la comunicación utilizando los atributos de la etiqueta html.";
+
+  
+}
+cambiarTexto(){
+  this.txtEvent="Maximiliano";
+}
 }
